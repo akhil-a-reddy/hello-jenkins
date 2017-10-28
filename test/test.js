5c95131152430d63f1d08c9f39c4186afaa4bec8
akhil <akhil@localhost.localdomain>
2017-10-28 11:42:39 -0400

    Add node app

--- test/test.js
+++ test/test.js
@@ -2,7 +2,7 @@ var request = require('supertest');
 var app = require('../app.js');
  
 describe('GET /', function() {
-  it('respond with hello worlda', function(done) {
-    request(app).get('/').expect('hello jenkins-stuff', done);
+  it('respond with hello world', function(done) {
+    request(app).get('/').expect('hello world', done);
   });
 });
